8ebc24698b127675b0cbd4d8dd12549d20e0adcf
Rachel <Rachel@Andreass-MacBook-Pro-Oct-2021.local>
2024-10-03 16:46:50 +0200

    Updating homework q4 and 5

---
+++ hw4/rb3028_hw4_q4.cpp
@@ -0,0 +1,49 @@
+/*HW 4, Q. 4*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+    int userInput, seqLength, originalLength, productOfInputs = 1;
+    double geomMean;
+
+    cout << "First version of program.\n";
+    cout << "Please enter the length of your sequence: ";
+    cin >> seqLength;
+
+    originalLength = seqLength;
+
+    cout << "Please enter your sequence of positive integers: ";
+
+    while (seqLength > 0){
+        cin >> userInput;
+        productOfInputs *= userInput;
+        seqLength--;
+    }
+
+    geomMean = pow(double(productOfInputs), (1.0/originalLength));
+    cout << "The geometric mean is: " << geomMean << endl;
+
+//Beginning of second version of the program. 
+    userInput = 1;
+    productOfInputs = 1;
+    
+    cout << "Second version of program.\n";
+    cout << "Please enter a non-empty sequence of positive integers, each one in a separate line. End your sequence by typing -1: ";
+    
+    do {
+        cin >> userInput;
+        if (userInput == -1)
+            break;
+        else
+            productOfInputs *= userInput;
+    } while (userInput != -1);
+
+    geomMean = pow(double(productOfInputs), (1.0/originalLength));
+    cout << "The geometric mean is: " << geomMean << endl;
+    
+    return 0;
+
+}
